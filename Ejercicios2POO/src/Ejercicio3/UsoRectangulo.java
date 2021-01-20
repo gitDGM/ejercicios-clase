6afde79c163d589bf63599f5bb17a00e9b1f3fd3
Diego Gómez <dgomezmo99@gmail.com>
2021-01-20 17:14:09 +0100

    UsoRectangulo class

---
+++ Ejercicios2POO/src/Ejercicio3/UsoRectangulo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ejercicio3;
+
+/**
+ *
+ * @author alumno
+ */
+public class UsoRectangulo {
+    public static void main(String[] args) {
+        Rectangulo r1 = new Rectangulo(1, 5, 10, 7);
+        
+        r1.imprimir();
+        r1.getArea();
+        r1.getPerimetro();
+    }
+}
