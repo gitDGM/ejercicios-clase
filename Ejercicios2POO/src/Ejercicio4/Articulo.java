b4317af9243d3761397eb231089b6f99cc9ad9a5
Diego Gómez <dgomezmo99@gmail.com>
2021-01-21 17:09:41 +0100

    Clase Articulo

--- Ejercicios2POO/src/Ejercicio4/Articulo.java
+++ Ejercicios2POO/src/Ejercicio4/Articulo.java
@@ -10,5 +10,50 @@ package Ejercicio4;
  * @author alumno
  */
 public class Articulo {
+    private String nombre;
+    private double precio;
+    private int iva;
+    private int cuantosQueda;
+
+    public Articulo(String nombre, double precio, int iva, int cuantosQueda) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.iva = iva;
+        this.cuantosQueda = cuantosQueda;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public int getIva() {
+        return iva;
+    }
+
+    public int getCuantosQueda() {
+        return cuantosQueda;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public void setIva(int iva) {
+        this.iva = iva;
+    }
+
+    public void setCuantosQueda(int cuantosQueda) {
+        this.cuantosQueda = cuantosQueda;
+    }
     
+    
+            
 }
