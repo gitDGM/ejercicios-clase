f49a137e7e0883e87c88c375319363d1ec468b88
Diego Gómez <dgomezmo99@gmail.com>
2021-01-26 18:41:25 +0100

    getter setters constructor fechas_horas

--- fechas_horas/src/FechaHora.java
+++ fechas_horas/src/FechaHora.java
@@ -18,8 +18,90 @@ public class FechaHora {
     private LocalDate fechaLocalDate;
     private LocalTime fechaLocalTime;
     private LocalDateTime fechaLocalDateTime;
-    
-    public FechaHora(int day, int month) {
-        
+
+    public FechaHora(int day, int month, int year, int hour, int minute, int seconds, LocalDate fechaLocalDate, LocalTime fechaLocalTime, LocalDateTime fechaLocalDateTime) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+        this.hour = hour;
+        this.minute = minute;
+        this.seconds = seconds;
+        this.fechaLocalDate = fechaLocalDate;
+        this.fechaLocalTime = fechaLocalTime;
+        this.fechaLocalDateTime = fechaLocalDateTime;
     }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+
+    public void setFechaLocalDate(LocalDate fechaLocalDate) {
+        this.fechaLocalDate = fechaLocalDate;
+    }
+
+    public void setFechaLocalTime(LocalTime fechaLocalTime) {
+        this.fechaLocalTime = fechaLocalTime;
+    }
+
+    public void setFechaLocalDateTime(LocalDateTime fechaLocalDateTime) {
+        this.fechaLocalDateTime = fechaLocalDateTime;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public LocalDate getFechaLocalDate() {
+        return fechaLocalDate;
+    }
+
+    public LocalTime getFechaLocalTime() {
+        return fechaLocalTime;
+    }
+
+    public LocalDateTime getFechaLocalDateTime() {
+        return fechaLocalDateTime;
+    }
+    
+    
 }
