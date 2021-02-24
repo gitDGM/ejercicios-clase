947496b561bc0f053383217156c21948492bf64c
Diego Gómez <dgomezmo99@gmail.com>
2021-02-24 15:48:39 +0100

    update

---
+++ Tema7/src/EjerciciosFicherosTexto/Ejercicio1/Ejercicio1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EjerciciosFicherosTexto.Ejercicio1;
+
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+/**
+ *
+ * @author alumno
+ */
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        
+    }
+}
