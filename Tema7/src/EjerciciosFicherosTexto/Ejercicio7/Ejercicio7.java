403c25dbd0310a736a81b4b1a67320a2adefccd0
Diego Gómez <dgomezmo99@gmail.com>
2021-02-25 17:13:19 +0100

    update

---
+++ Tema7/src/EjerciciosFicherosTexto/Ejercicio7/Ejercicio7.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EjerciciosFicherosTexto.Ejercicio7;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author alumno
+ */
+public class Ejercicio7 {
+    public final static String RUTA_ACTUAL = "src/EjerciciosFicherosTexto/Ejercicio7/";
+    public final static int CANTIDAD_LINEAS = 24;
+    public static void main(String[] args) {
+        File fichero = new File(RUTA_ACTUAL + "texto.txt");
+    }
+    
+    public void mostrarParteFichero(File fichero) {
+        FileReader filereader = null;
+        BufferedReader lector = null;
+        
+        try {
+            filereader = new FileReader(fichero);
+            lector = new BufferedReader(filereader);    
+            
+            
+            
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            try {
+                if (filereader != null) filereader.close();
+                if (lector != null) lector.close();
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());                    
+            }
+        }
+    }
+}
