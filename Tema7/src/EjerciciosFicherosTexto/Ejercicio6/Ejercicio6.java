8a7f2484a848be8768634d3b411fec7b6808f348
Diego Gómez <dgomezmo99@gmail.com>
2021-02-25 16:59:14 +0100

    update

--- Tema7/src/EjerciciosFicherosTexto/Ejercicio6/Ejercicio6.java
+++ Tema7/src/EjerciciosFicherosTexto/Ejercicio6/Ejercicio6.java
@@ -5,13 +5,68 @@
  */
 package EjerciciosFicherosTexto.Ejercicio6;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 
 /**
  *
  * @author alumno
  */
 public class Ejercicio6 {
+    private static final Scanner sc = new Scanner(System.in);
+    
     public static void main(String[] args) {
-        
+        LibroFirmas libroFirmas = new LibroFirmas();
+        int opcion;
+        do {
+            System.out.println("##########################");
+            System.out.println("1.- Insertar firma");
+            System.out.println("2.- Mostrar libro de firmas");
+            System.out.println("0.- Salir");
+            System.out.println("##########################");
+            opcion = introducirNumero("Elige una opción:");
+            switch(opcion) {
+                case 1:
+                    insertarFirma(libroFirmas);
+                    break;
+                case 2:
+                    libroFirmas.mostrar();
+                    break;
+                case 0:
+                    break;
+                default:
+                    break;
+            }
+        } while (opcion != 0);       
+    }
+    
+    static void insertarFirma(LibroFirmas libroFirmas) {
+        System.out.println("Introduce el nombre de la firma:");
+        String nombre = sc.nextLine();
+        if (!nombre.equals("")) {
+            libroFirmas.addFirma(nombre);            
+        } else {
+            System.err.println("ATENCIÓN: No puedes añadir una firma vacía.");
+        }
+    }
+    
+    static int introducirNumero(String msg) {
+        Scanner entrada = new Scanner(System.in);
+        int numero = 0;        
+        boolean noValido;
+        do {
+            System.out.println(msg);
+            try {
+                numero = entrada.nextInt();
+                noValido = false;
+            } catch (InputMismatchException ex) {
+                System.err.println("Debe ser un número decimal.");
+                noValido = true;
+                entrada.next();
+            }
+        } while (noValido);
+
+        return numero;
     }
 }
--- Tema7/src/EjerciciosFicherosTexto/Ejercicio6/LibroFirmas.java
+++ Tema7/src/EjerciciosFicherosTexto/Ejercicio6/LibroFirmas.java
@@ -6,17 +6,30 @@
 package EjerciciosFicherosTexto.Ejercicio6;
 
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 /**
  *
  * @author alumno
  */
 public class LibroFirmas {
-    private final ArrayList<Firma> firmas = new ArrayList();
+    private static final String RUTA = "src/EjerciciosFicherosTexto/Ejercicio6/";
+    private final ArrayList<Firma> firmas;
+    
+    public LibroFirmas() {
+        firmas = new ArrayList();
+        cargarFicheroFirmas();
+    }
     
     public void addFirma(String nombre) {
         if (!comprobarRepetido(nombre)) {
             firmas.add(new Firma(nombre));
+            escribirFicheroFirma(nombre);
         } else {
             System.err.println("ATENCIÓN: Firma repetida.");
         }
@@ -24,9 +37,61 @@ public class LibroFirmas {
     
     public void mostrar() {
         int index = 1;
+        System.out.println("\n###### LIBRO DE FIRMAS ######");
         for (Firma firma : firmas) {
             System.out.println(index + ".- " + firma.getNombre());
             index++;
+        }
+    }
+    
+    private void escribirFicheroFirma(String nombreFirma) {
+        File fichero = new File(RUTA + "firmas.txt");
+        FileWriter filewriter = null;
+        PrintWriter escritor = null;
+        
+        try {         
+            filewriter = new FileWriter(fichero, true);
+            escritor = new PrintWriter(filewriter);
+            
+            escritor.println(nombreFirma);
+            
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            try {
+                if (filewriter != null) filewriter.close();
+                if (escritor != null) escritor.close();
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());                    
+            }
+        }
+    }
+    
+    private void cargarFicheroFirmas() {
+        File fichero = new File(RUTA + "firmas.txt");
+        
+        FileReader filereader = null;
+        BufferedReader lector = null;
+              
+        try {
+            filereader = new FileReader(fichero);
+            lector = new BufferedReader(filereader);        
+            
+            String firmaLeida = lector.readLine();
+            while (firmaLeida != null) {   
+                firmas.add(new Firma(firmaLeida));
+                firmaLeida = lector.readLine();
+            }
+            
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            try {
+                if (filereader != null) filereader.close();
+                if (lector != null) lector.close();
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());                    
+            }
         }
     }
     
