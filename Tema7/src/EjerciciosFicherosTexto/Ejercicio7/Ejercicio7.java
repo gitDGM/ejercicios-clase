286af7c698da868a4fb88de7588c0ae313378ef4
Diego Gómez <dgomezmo99@gmail.com>
2021-03-02 18:51:47 +0100

    update

--- Tema7/src/EjerciciosFicherosTexto/Ejercicio7/Ejercicio7.java
+++ Tema7/src/EjerciciosFicherosTexto/Ejercicio7/Ejercicio7.java
@@ -11,27 +11,42 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 /**
  *
  * @author alumno
  */
 public class Ejercicio7 {
+    
     public final static String RUTA_ACTUAL = "src/EjerciciosFicherosTexto/Ejercicio7/";
     public final static int CANTIDAD_LINEAS = 24;
+    public final static Scanner sc = new Scanner(System.in);
+    
     public static void main(String[] args) {
         File fichero = new File(RUTA_ACTUAL + "texto.txt");
+        
+        mostrarParteFichero(fichero);
     }
     
-    public void mostrarParteFichero(File fichero) {
+    static void mostrarParteFichero(File fichero) {
+        
         FileReader filereader = null;
         BufferedReader lector = null;
         
         try {
+            
             filereader = new FileReader(fichero);
-            lector = new BufferedReader(filereader);    
+            lector = new BufferedReader(filereader);
             
-            
+            String cadenaLeida = lector.readLine();
+            while (cadenaLeida != null) {
+                for (int i = 0; i < CANTIDAD_LINEAS && cadenaLeida != null; i++) {
+                    System.out.println(cadenaLeida);
+                    cadenaLeida = lector.readLine();
+                }
+                sc.nextLine();
+            }
             
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
