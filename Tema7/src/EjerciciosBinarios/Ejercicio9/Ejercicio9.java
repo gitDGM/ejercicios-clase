85f9d563f3bf48d6ea740d814b9d877aad596dda
Diego Gómez <dgomezmo99@gmail.com>
2021-03-19 17:47:56 +0100

    update

--- Tema7/src/EjerciciosBinarios/Ejercicio9/Ejercicio9.java
+++ Tema7/src/EjerciciosBinarios/Ejercicio9/Ejercicio9.java
@@ -5,9 +5,12 @@
  */
 package EjerciciosBinarios.Ejercicio9;
 
+import java.io.EOFException;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 /**
@@ -17,7 +20,52 @@ import java.io.ObjectOutputStream;
 public class Ejercicio9 {
     static final int CANTIDAD_NUMS = 10;
     public static void main(String[] args) {
-        // TODO: ORDENAR
+        String ruta = "src/EjerciciosBinarios/Ejercicio9/data.dat";
+        escribirEnFicheroBinario(ruta);
+        mostrarTabla(leerFicheroBinario(ruta));
+    }
+    
+    static void mostrarTabla(int[] tabla) {
+        for (int numero : tabla ) {
+            System.out.print(numero + " ");
+        }
+        System.out.println();
+    }
+    
+    static int[] bubbleSortArray(int[] tabla) {
+        for(int i = 0; i < tabla.length - 1; i++) {
+            for (int k = 0; k < tabla.length - 1 - i; k++) {
+                if (tabla[k] > tabla[k + 1]) {
+                    int temp = tabla[k];
+                    tabla[k] = tabla[k + 1];
+                    tabla[k + 1] = temp;
+                }
+            }
+        }
+        
+        return tabla;
+    }
+    
+    static int[] leerFicheroBinario(String ruta)  {
+        ObjectInputStream flujoEntrada = null;
+        int[] tabla = new int[CANTIDAD_NUMS];        
+        try {        
+            flujoEntrada = new ObjectInputStream(new FileInputStream(ruta)); 
+            tabla = (int[]) flujoEntrada.readObject();            
+        } catch (FileNotFoundException ex) {
+            System.err.println("Error!!! El fichero no existe.");
+        } catch (IOException | ClassNotFoundException ex) {
+            System.err.println(ex.getMessage());
+        } finally {
+            try {
+                if(flujoEntrada != null) {
+                    flujoEntrada.close();
+                } 
+            } catch (IOException ex) {
+                System.err.println("Error al cerrar el fichero.");
+            }
+        }
+        return tabla;
     }
     
     static void escribirEnFicheroBinario(String ruta) {
@@ -28,6 +76,7 @@ public class Ejercicio9 {
             for (int i = 0; i < CANTIDAD_NUMS; i++) {
                 tabla[i] = (int)(Math.random() * 100);
             }
+            tabla = bubbleSortArray(tabla);
             flujoSalida.writeObject(tabla);
         } catch (FileNotFoundException ex) {
             System.err.println("Error!!! El fichero no existe.");
