d65a01c8876eff09c266a624937b6b4083548449
Diego Gómez <dgomezmo99@gmail.com>
2021-05-06 16:46:39 +0200

    update

---
+++ Tema8/src/ComparadorPersonalizado/ComparadorPersonalizado.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ComparadorPersonalizado;
+
+import java.util.Comparator;
+
+/**
+ *
+ * @author alumno
+ */
+public class ComparadorPersonalizado implements Comparator {
+
+    @Override
+    public int compare(Object arg0, Object arg1) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
