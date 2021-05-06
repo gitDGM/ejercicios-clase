5f90d0b4185bc92bb7d591778bc4224cc9ec558b
Diego Gómez <dgomezmo99@gmail.com>
2021-05-06 16:24:48 +0200

    update

--- Tema8/src/EstructurasDinamicas/Ejercicio3/NumerosController.java
+++ Tema8/src/EstructurasDinamicas/Ejercicio3/NumerosController.java
@@ -5,22 +5,109 @@
  */
 package EstructurasDinamicas.Ejercicio3;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
 
 /**
  *
  * @author alumno
  */
 public class NumerosController {
-    private final Queue<Integer> colaNumeros;
-    private final Stack<Integer> pilaNumeros;
-    private final LinkedList<Integer> listaNumeros;
+    private static final int N_NUMS = 10;
+    private final TreeSet<Integer> arbolNumeros;
+    private final HashSet<Integer> hashNumeros;
+    private final LinkedHashSet<Integer> linkedHashNumeros;
     
     public NumerosController() {
-        colaNumeros = new LinkedList();
-        pilaNumeros = new Stack();
-        listaNumeros = new LinkedList();
+        arbolNumeros = new TreeSet();
+        hashNumeros = new HashSet();
+        linkedHashNumeros = new LinkedHashSet();
+        
+        for (int i = 0; i < N_NUMS; i++) {
+            addNumero(generarNumero());
+        }
     }
+    
+    public void renovarNumeros() {
+        vaciarEstructuras();
+        for (int i = 0; i < N_NUMS; i++) {
+            addNumero(generarNumero());
+        }
+    }
+    
+    public void mostrarArbol() {
+        System.out.println("\n\n#### Estructura TreeSet ####\n");
+        Iterator<Integer> it = arbolNumeros.iterator();
+        while (it.hasNext()) {
+            System.out.print(it.next() + "\t");
+        }
+        System.out.println("\n");
+    }
+    
+    public void mostrarHash() {
+        System.out.println("\n\n#### Estructura HashSet ####\n");
+        Iterator<Integer> it = hashNumeros.iterator();
+        while (it.hasNext()) {
+            System.out.print(it.next() + "\t");
+        }
+        System.out.println("\n");
+    }
+    
+    public void mostrarLinkedHash() {
+        System.out.println("\n\n#### Estructura LinkedHashSet ####\n");
+        Iterator<Integer> it = linkedHashNumeros.iterator();
+        while (it.hasNext()) {
+            System.out.print(it.next() + "\t");
+        }
+        System.out.println("\n");
+    }
+    
+    private void addNumero(int numero) {
+        arbolNumeros.add(numero);
+        hashNumeros.add(numero);
+        linkedHashNumeros.add(numero);
+    }
+    
+    private int generarNumero() {
+        
+        ////
+        // NO ES NECESARIO PORQUE LA PROPIA CLASE NO PERMITE DUPLICADOS
+        /*         
+        int numero;
+        
+        do {            
+            numero = (int) (Math.random() * 100);
+        } while (checkNum(numero));
+        */     
+        ////
+        
+        
+        
+        return (int) (Math.random() * 100);
+    }
+    
+    private void vaciarEstructuras() {
+        arbolNumeros.clear();
+        hashNumeros.clear();
+        linkedHashNumeros.clear();
+    }
+    
+    
+    ////
+    // NO ES NECESARIO PORQUE LA PROPIA CLASE NO PERMITE DUPLICADOS
+    /* 
+    private boolean checkNum(int numero) {
+        boolean existe = false;
+        
+        Iterator<Integer> it = linkedHashNumeros.iterator();
+        while (it.hasNext() && !existe) {            
+            existe = numero == it.next();
+        }
+        
+        return existe;
+    }
+    */     
+    ////
 }
--- Tema8/src/EstructurasDinamicas/Ejercicio3/NumerosPrincipal.java
+++ Tema8/src/EstructurasDinamicas/Ejercicio3/NumerosPrincipal.java
@@ -5,12 +5,64 @@
  */
 package EstructurasDinamicas.Ejercicio3;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * @author alumno
  */
 public class NumerosPrincipal {
     public static void main(String[] args) {
-        
+        NumerosController numerosController = new NumerosController();
+        int opcion;
+        do {
+            System.out.println("##########################");
+            System.out.println("1.- Mostrar TreeSet");
+            System.out.println("2.- Mostrar HashSet");
+            System.out.println("3.- Mostrar LinkedHashSet");
+            System.out.println("4.- Generar nuevos números");
+            System.out.println("0.- Salir");
+            System.out.println("##########################");
+            opcion = introducirNumero("Elige una opción:");
+            switch(opcion) {
+                case 1:
+                    numerosController.mostrarArbol();
+                    break;
+                case 2:
+                    numerosController.mostrarHash();
+                    break;
+                case 3:
+                    numerosController.mostrarLinkedHash();
+                    break;
+                case 4:
+                    numerosController.renovarNumeros();
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.err.println("ERROR: Elige una opción válida.");
+                    break;
+            }
+        } while (opcion != 0);       
+    }
+    
+    static int introducirNumero(String msg) {
+        Scanner sc = new Scanner(System.in);
+        int numero = 0;        
+        boolean noValido;
+        do {
+            System.out.println(msg);
+            try {
+                numero = sc.nextInt();
+                noValido = false;
+            } catch (InputMismatchException ex) {
+                System.err.println("Debe ser un número decimal.");
+                noValido = true;
+                sc.next();
+            }
+        } while (noValido);
+
+        return numero;
     }
 }
