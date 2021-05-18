0ba0f07633ca916276b2fb304dc1f43568c3d097
Diego Gómez <dgomezmo99@gmail.com>
2021-05-18 19:59:21 +0200

    update

--- Tema9/src/Ejercicio2/Principal.java
+++ Tema9/src/Ejercicio2/Principal.java
@@ -5,6 +5,9 @@
  */
 package Ejercicio2;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * @author alumno
@@ -16,12 +19,13 @@ public class Principal {
         int opcion;
         do {
             System.out.println("##########################");
-            System.out.println("1.- Insertar fabricante");
-            System.out.println("2.- Insertar producto");
-            System.out.println("3.- Eliminar producto");
-            System.out.println("4.- Modificar producto");
-            System.out.println("5.- Mostrar producto de un fabricante");
-            System.out.println("6.- Generar fichero de texto (Productos >= 500€)");
+            System.out.println("1.- Insertar funcionario");
+            System.out.println("2.- Insertar contribuyente");
+            System.out.println("3.- Eliminar contribuyente");
+            System.out.println("4.- Modificar importe de un contribuyente");
+            System.out.println("5.- Mostrar contribuyentes atendidos por un funcionario");
+            System.out.println("6.- Generar listado de poblaciones de los funcionarios");
+            System.out.println("7.- Generar fichero de contribuyentes en una población");
             System.out.println("0.- Salir");
             System.out.println("##########################");
             opcion = introducirNumero("Elige una opción:");
@@ -32,5 +36,50 @@ public class Principal {
                     break;
             }
         } while (opcion != 0);
+    }
+
+    static String introducirCadena(String msg) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(msg);
+
+        return sc.nextLine();
+    }
+
+    static int introducirNumero(String msg) {
+        Scanner sc = new Scanner(System.in);
+        int numero = 0;
+        boolean noValido;
+        do {
+            System.out.println(msg);
+            try {
+                numero = sc.nextInt();
+                noValido = false;
+            } catch (InputMismatchException ex) {
+                System.err.println("Debe ser un número decimal.");
+                noValido = true;
+                sc.next();
+            }
+        } while (noValido);
+
+        return numero;
+    }
+
+    static double introducirNumeroDouble(String msg) {
+        Scanner sc = new Scanner(System.in);
+        double numero = 0;
+        boolean noValido;
+        do {
+            System.out.println(msg);
+            try {
+                numero = sc.nextDouble();
+                noValido = false;
+            } catch (InputMismatchException ex) {
+                System.err.println("Debe ser un número decimal.");
+                noValido = true;
+                sc.next();
+            }
+        } while (noValido);
+
+        return numero;
     }
 }
