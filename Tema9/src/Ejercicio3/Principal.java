160d08ff4418e13ca39c466cfa8c45038e7388cd
Diego Gómez <dgomezmo99@gmail.com>
2021-05-21 17:45:23 +0200

    update

--- Tema9/src/Ejercicio3/Principal.java
+++ Tema9/src/Ejercicio3/Principal.java
@@ -5,6 +5,7 @@
  */
 package Ejercicio3;
 
+import java.time.LocalDate;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -28,6 +29,7 @@ public class Principal {
             opcion = introducirNumero("Elige una opción:");
             switch(opcion) {
                 case 1:
+                    opcionInsertarAninal();
                     break;
                 case 0:
                     break;
@@ -35,6 +37,95 @@ public class Principal {
                     break;
             }
         } while (opcion != 0);
+    }
+
+    static void opcionInsertarAninal() {
+        int opcion;
+        do {
+            System.out.println("##########################");
+            System.out.println("1.- Perro");
+            System.out.println("2.- Gato");
+            System.out.println("3.- Loro");
+            System.out.println("4.- Canario");
+            System.out.println("0.- Salir");
+            System.out.println("##########################");
+            opcion = introducirNumero("Elige un tipo de animal:");
+            switch(opcion) {
+                case 1:
+                    insertarPerro();
+                    break;
+                case 2:
+                    insertarGato();
+                    break;
+                case 3:
+                    insertarLoro();
+                    break;
+                case 4:
+                    insertarCanario();
+                    break;
+                case 0:
+                    break;
+                default:
+                    break;
+            }
+        } while (opcion != 0);
+
+    }
+
+    static void insertarPerro() {
+        String nombre = introducirCadena("Introduce el nombre del perro:");
+        int edad = introducirNumero("Introduce la edad del perro:");
+        boolean estado = elegirEstado("Está vivo? (S/N):");
+        LocalDate fechaNacimiento = LocalDate.of(
+                introducirNumero("Introduce el año (aaaa): "),
+                introducirNumero("Introduce el mes (mm): "),
+                introducirNumero("Introduce el día (dd):")
+        );
+        String raza = introducirCadena("Introduce la raza del perro:");
+        boolean pulgas = elegirEstado("Tiene pulgas vivo? (S/N):");
+    }
+
+    static void insertarGato() {
+        String nombre = introducirCadena("Introduce el nombre del gato:");
+        int edad = introducirNumero("Introduce la edad del gato:");
+        boolean estado = elegirEstado("Está vivo? (S/N):");
+        LocalDate fechaNacimiento = LocalDate.of(
+                introducirNumero("Introduce el año (aaaa): "),
+                introducirNumero("Introduce el mes (mm): "),
+                introducirNumero("Introduce el día (dd):")
+        );
+        String raza = introducirCadena("Introduce el colo del gato:");
+        boolean peloLargo = elegirEstado("Tiene pelo largo? (S/N):");
+    }
+
+    static void insertarLoro() {
+        String nombre = introducirCadena("Introduce el nombre del loro:");
+        int edad = introducirNumero("Introduce la edad del loro:");
+        boolean estado = elegirEstado("Está vivo? (S/N):");
+        LocalDate fechaNacimiento = LocalDate.of(
+                introducirNumero("Introduce el año (aaaa): "),
+                introducirNumero("Introduce el mes (mm): "),
+                introducirNumero("Introduce el día (dd):")
+        );
+        boolean pico = elegirEstado("Tiene pico? (S/N):");
+        boolean vuela = elegirEstado("Puede volar? (S/N):");
+        String raza = introducirCadena("Introduce el origen del loro:");
+        boolean habla = elegirEstado("Puede hablar? (S/N):");
+    }
+
+    static void insertarCanario() {
+        String nombre = introducirCadena("Introduce el nombre del canario:");
+        int edad = introducirNumero("Introduce la edad del canario:");
+        boolean estado = elegirEstado("Está vivo? (S/N):");
+        LocalDate fechaNacimiento = LocalDate.of(
+                introducirNumero("Introduce el año (aaaa): "),
+                introducirNumero("Introduce el mes (mm): "),
+                introducirNumero("Introduce el día (dd):")
+        );
+        boolean pico = elegirEstado("Tiene pico? (S/N):");
+        boolean vuela = elegirEstado("Puede volar? (S/N):");
+        String color = introducirCadena("Introduce el color del canario:");
+        boolean canta = elegirEstado("Puede cantar? (S/N):");
     }
 
     static String introducirCadena(String msg) {
@@ -82,7 +173,7 @@ public class Principal {
         return numero;
     }
 
-    static boolean elegirPagar(String msg) {
+    static boolean elegirEstado(String msg) {
         Scanner sc = new Scanner(System.in);
         boolean opcion = true;
         boolean noValido;
---
+++ Tema9/src/Ejercicio3/mascotas-bck.sql
@@ -0,0 +1,62 @@
+-- MySQL dump 10.19  Distrib 10.3.29-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: mascotas
+-- ------------------------------------------------------
+-- Server version	10.3.29-MariaDB-0ubuntu0.20.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `mascotas`
+--
+
+DROP TABLE IF EXISTS `mascotas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mascotas` (
+  `idMascota` int(11) NOT NULL,
+  `edad` int(3) NOT NULL,
+  `vivo` tinyint(1) NOT NULL,
+  `fecha_nacimiento` date NOT NULL,
+  `raza` varchar(100) DEFAULT NULL,
+  `pulgas` tinyint(1) DEFAULT NULL,
+  `color` varchar(100) DEFAULT NULL,
+  `pelo_largo` tinyint(1) DEFAULT NULL,
+  `pico` tinyint(1) DEFAULT NULL,
+  `vuela` tinyint(1) DEFAULT NULL,
+  `origen` varchar(150) DEFAULT NULL,
+  `habla` tinyint(1) DEFAULT NULL,
+  `canta` tinyint(1) DEFAULT NULL,
+  `tipo` int(11) NOT NULL,
+  PRIMARY KEY (`idMascota`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mascotas`
+--
+
+LOCK TABLES `mascotas` WRITE;
+/*!40000 ALTER TABLE `mascotas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mascotas` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-05-21 17:45:07
